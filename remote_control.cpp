508c704daf96f2c2b1a5801949fa26b7b06de51f
Christophe de Dinechin <christophe@dinechin.org>
2019-03-22 13:07:11 +0100

    Automatic copyright header adjustment

--- remote_control.cpp
+++ remote_control.cpp
@@ -1,22 +1,41 @@
-// ****************************************************************************
-//  remote_control.cpp                                             Tao project
-// ****************************************************************************
-// 
-//   File Description:
-// 
+// *****************************************************************************
+// remote_control.cpp                                              Tao3D project
+// *****************************************************************************
+//
+// File description:
+//
 //    Functions control the Tao process remotely.
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// ****************************************************************************
-//  (C) 2012 Jérôme Forissier <jerome@taodyne.com>
-//  (C) 2012 Taodyne SAS
-// ****************************************************************************
+//
+//
+//
+//
+//
+//
+//
+//
+// *****************************************************************************
+// This software is licensed under the GNU General Public License v3
+// (C) 2012, Baptiste Soulisse <baptiste.soulisse@taodyne.com>
+// (C) 2012, Catherine Burvelle <catherine@taodyne.com>
+// (C) 2012,2019, Christophe de Dinechin <christophe@dinechin.org>
+// (C) 2012, Jérôme Forissier <jerome@taodyne.com>
+// *****************************************************************************
+// This file is part of Tao3D
+//
+// Tao3D is free software: you can r redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Tao3D is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Tao3D, in a file named COPYING.
+// If not, see <https://www.gnu.org/licenses/>.
+// *****************************************************************************
 
 #include "remote_control.h"
 #include "hook.h"
@@ -91,9 +110,9 @@ XL::Tree_p remoteControlWriteln(XL::Tree_p self, int id, text msg, bool once)
 
 
 // ============================================================================
-// 
+//
 //    Module glue
-// 
+//
 // ============================================================================
 
 int module_init(const Tao::ModuleApi *api, const Tao::ModuleInfo *)
